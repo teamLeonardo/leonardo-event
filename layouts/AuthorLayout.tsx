00b0915996b7984ea10a357fc37697c992a052bf
leonardo sifuentes <leonardosm3.14@gmail.com>
2023-11-21 02:00:52 -0500

    linea del tiempo

--- layouts/AuthorLayout.tsx
+++ layouts/AuthorLayout.tsx
@@ -13,7 +13,7 @@ export default function AuthorLayout({ children, content }: Props) {
 
   return (
     <>
-      <div className="divide-y divide-gray-200 dark:divide-gray-700">
+      <div className="divide-y divide-gray-200 dark:divide-gray-700 mb-10 divide-double">
         <div className="space-y-2 pb-8 pt-6 md:space-y-5">
           <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
             Sobre Mi
@@ -41,10 +41,94 @@ export default function AuthorLayout({ children, content }: Props) {
               <SocialIcon kind="twitter" href={twitter} />
             </div>
           </div>
-          <div className="prose max-w-none pb-8 pt-8 dark:prose-invert xl:col-span-2">
+          <div className="prose max-w-none text-gray-950 dark:text-gray-200 pb-8 pt-8 dark:prose-invert xl:col-span-2">
             {children}
           </div>
         </div>
+
+        <div className='pt-6'>
+          <h2 className="text-xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:leading-14">
+            Experencia Laboral
+          </h2>
+          <ul className="timeline timeline-vertical timeline-snap-icon timeline-compact">
+            {
+              [
+                {
+                  puesto: "Frontend Desarrollado",
+                  empresa: "SOEN SAC.",
+                  fecha: "Enero 2023 - Agosto 2023",
+                  contenido: [
+                    "He contribuido en el desarrollo de sistemas completos de gestión, abarcando la administración de pedidos, productos y clientes.",
+                    "He implementado soluciones de seguimiento en tiempo real para pedidos, aprovechando la sinergia entre Google Maps y Firebase.",
+                    "Adicionalmente, he logrado una integración impecable de las APIs de Google y Facebook, mejorando significativamente la fluidez y eficiencia del proceso de inicio de sesión."
+                  ]
+                },
+                {
+                  puesto: "E-commerce frontend Desarrollador",
+                  empresa: "Avatar Global",
+                  fecha: "Julio 2021 - Diciembre 2022",
+                  contenido: [
+                    "Contribución en todas las fases del desarrollo con la integración de nuevas funcionalidades.",
+                    "Mantenimiento y mejora de  aplicaciones existentes.",
+                    "Investigación de capacidades nativas de HCL Commerce para la creación de una nueva tienda mediante React.js."
+                  ]
+                },
+                {
+                  puesto: "Desarrollador web",
+                  empresa: "PLAYTEC",
+                  fecha: "Enero 2023 - Agosto 2023",
+                  contenido: [
+                    "Realización de maquetación según pautas de diseño del equipo.",
+                    "Creación de componentes reutilizables en React.",
+                    "Utilización de Redux para gestionar el estado de la aplicación.",
+                    "Integración de protocolos de socket y WebRTC en el cliente para mejorar la        comunicación y funcionalidad."
+                  ]
+                },
+                {
+                  puesto: "Desarrollador PHP y Visual Basic .NET",
+                  empresa: "QUIPU S.A",
+                  fecha: "Mayo 2019 - Enero 2020",
+                  contenido: [
+                    "Encargado del mantenimiento de software contable en Visual Basic y del desarrollo de soluciones web de gestión en PHP.",
+                    "Responsable de mantener bases de datos relacionales para ambos proyectos y de optimizar consultas y rendimiento en la aplicación web de gestión."
+                  ]
+                }
+              ].map((el) => (
+                <li>
+                  <div className="timeline-start">
+                    <div className="text-sm font-black ">
+                      {el.puesto}
+                    </div>
+                    <time className="font-mono italic">
+                      {el.fecha}
+                    </time>
+                  </div>
+                  <div className="timeline-middle">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="h-5 w-5"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clipRule="evenodd" /></svg>
+                  </div>
+                  <div className="timeline-box timeline-end text-start bg-white dark:bg-[#111728]">
+                    <div className="text-lg font-black">
+                      {el.empresa}
+                    </div>
+                    <div className='dark:text-gray-400'>
+                      {el.contenido.map((cont) => (
+                        <>
+                          <span>
+                            {cont}
+                          </span>
+                          <br />
+                        </>
+                      ))}
+                    </div>
+                  </div>
+                  <hr />
+                </li>
+              ))
+            }
+
+          </ul>
+        </div>
+
       </div>
     </>
   )
